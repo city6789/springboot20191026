6fd0c7a45dbbe224ae05715211c469ccc9cff41f
tomorrow2016 <hesg2012@126.com>
2019-10-27 00:37:26 +0800

    hsg的提交代码

--- src/main/java/cn/hsy/test/springboot20191026/Springboot20191026Application.java
+++ src/main/java/cn/hsy/test/springboot20191026/Springboot20191026Application.java
@@ -9,5 +9,7 @@ public class Springboot20191026Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot20191026Application.class, args);
 	}
+	
+	//hsgµÄ¸Ä¶¯----------------------ssss
 
 }
---
+++ src/main/java/cn/hsy/test/springboot20191026/Test.java
@@ -0,0 +1,13 @@
+package cn.hsy.test.springboot20191026;
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("xxxxxxxxxxxxxxxxxx");
+		//UUUUUUUUUUUUUUUUUU
+	}
+
+
+}
