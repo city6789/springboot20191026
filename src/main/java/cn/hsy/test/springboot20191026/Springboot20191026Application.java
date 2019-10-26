cfa9e683ad5bc36e4c1ef78dd90ecfd2f926d31b
heshiyuan163 <15801636814@163.com>
2019-10-26 23:31:15 +0800

    init  prokect

---
+++ src/main/java/cn/hsy/test/springboot20191026/Springboot20191026Application.java
@@ -0,0 +1,13 @@
+package cn.hsy.test.springboot20191026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot20191026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot20191026Application.class, args);
+	}
+
+}
---
+++ src/test/java/cn/hsy/test/springboot20191026/Springboot20191026ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hsy.test.springboot20191026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot20191026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
